75e690fc62d7da072c596360088ea9ac8b565870
Juliano Galhardo de Oliveira <julianogalhardo@192.168.1.3>
2025-11-06 21:45:18 -0300

    feat: Banco de dados -  tratando env com Zod

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,10 @@
-import "dotenv/config"
 import Knex, { type Knex as KnexType } from 'knex'
-
-if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL env not found.')
-}
+import { env } from './env/index.js'
 
 export const config: KnexType.Config = {
     client: 'sqlite',
     connection: {
-        filename: process.env.DATABASE_URL,
+        filename: env.DATABASE_URL,
     },
     useNullAsDefault: true,
     migrations: {
