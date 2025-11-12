d2f8cfa93829b13f2ed9ba3a57c7ae682db3a403
Juliano Galhardo de Oliveira <julianogalhardo@192.168.1.3>
2025-11-11 21:42:42 -0300

    feat: Utilizando cookies no Fastify

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,63 +1,76 @@
 import { FastifyInstance } from "fastify";
-import { z } from 'zod'
+import { z } from "zod";
 import { knex } from "../database.js";
 import { randomUUID } from "crypto";
 
 // Request Body: De onde vem as informações que servem para criar ou modificar um recurso
 
-export async function transactionsRoutes(app: FastifyInstance) {
+// Cookies -> Formas de manter contexto entre requisições (Salva seu id para conseguir validar se a mesma pessoa fez tais requisições) = meio que já salvar o usuário
 
-  app.get('/', async () => {
-    const transactions = await knex ('transactions').select()
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.get("/", async () => {
+    const transactions = await knex("transactions").select();
 
     return {
       transactions,
-    }
-  })
+    };
+  });
 
   // http://localhost:3333/transactions/O id do usuário
 
-  app.get('/:id', async (request) => {
+  app.get("/:id", async (request) => {
     const getTransactionParamsSchema = z.object({
       id: z.uuid(),
-    })
+    });
 
-    const { id } = getTransactionParamsSchema.parse(request.params)
+    const { id } = getTransactionParamsSchema.parse(request.params);
 
-    const transaction = await knex('transactions').where('id', id).first()
+    const transaction = await knex("transactions").where("id", id).first();
 
-    return { transaction }
-  })
+    return { transaction };
+  });
 
-  app.get('/summary', async () => {
-    const summary = await knex('transactions').sum('amount', {as: 'amount'}).first()
+  app.get("/summary", async () => {
+    const summary = await knex("transactions")
+      .sum("amount", { as: "amount" })
+      .first();
 
-    return { summary }
-  })
+    return { summary };
+  });
 
   app.post("/", async (request, reply) => {
+    console.log("🚀 Entrou na rota POST /transactions");
 
     const createTransactionBodySchema = z.object({
-        title: z.string(),
-        amount: z.number(),
-        type: z.enum(['credit', 'debit']),
-    })
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
 
     const { title, amount, type } = createTransactionBodySchema.parse(
-        request.body,
-    )
+      request.body
+    );
 
-    await knex('transactions').insert({
-        id: randomUUID(),
-        title,
-        amount: type === 'credit' ? amount : amount * -1,
+    let sessionId = request.cookies.sessionId;
+    console.log('Cookies recebidos:', request.cookies)
 
-    })
+    if (!sessionId) {
+      sessionId = randomUUID();
+      console.log('Criando novo cookie com sessionId:', sessionId)
 
-    // Query Builders
+      reply.cookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      });
+    }
 
-    //HTTP Codes
+    await knex("transactions").insert({
+      id: randomUUID(),
+      title,
+      amount: type === "credit" ? amount : amount * -1,
+      session_id: sessionId,
+    });
 
-    return reply.status(201).send()
+    return reply.status(201).send();
   });
 }
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,23 @@
-import fastify from 'fastify'
-import { env } from './env/index.js'
-import { transactionsRoutes } from './routes/transactions.js'
+import fastify from "fastify";
+import cookie from "@fastify/cookie";
 
-const app = fastify()
+import { env } from "./env/index.js";
+import { transactionsRoutes } from "./routes/transactions.js";
 
+const app = fastify();
+
+app.register(cookie, {
+  secret: "uma_chave_secreta_segura",
+  hook: "onRequest",
+});
 app.register(transactionsRoutes, {
-    prefix: 'transactions',
-})
+  prefix: "transactions",
+});
 
-app.listen({
+app
+  .listen({
     port: env.PORT,
-}).then(() => {
-    console.log('HTTP Server is Running!')
-})
+  })
+  .then(() => {
+    console.log("HTTP Server is Running!");
+  });
